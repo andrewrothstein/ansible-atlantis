1f3a198e4d3ac1c487f724461efca1a4bc6890a4
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-03-04 11:19:27 -0500

    atlantis:0.18.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -13,7 +13,7 @@ dl()
     local lfile=$DIR/atlantis_${platform}_${ver}.zip
     if [ ! -e $lfile ];
     then
-       wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform `sha256sum $lfile | awk '{print $1}'`
@@ -32,4 +32,4 @@ dl_ver() {
     dl $ver windows amd64
 }
 
-dl_ver ${1:-v0.18.2}
+dl_ver ${1:-v0.18.3}
